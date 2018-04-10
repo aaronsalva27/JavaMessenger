7098bef7fefe4ff6d939f599c40d3fc031f28086
aaronsalva27 <aaronsalva27@gmail.com>
2018-04-10 19:02:56 +0200

    [SERVER] import clcinet module

--- server/JavaMessengerServer/index.js
+++ server/JavaMessengerServer/index.js
@@ -1,18 +1,17 @@
-var net = require('net');
+const client = require("./clients");
 
+
+var net = require('net');
 var HOST = '127.0.0.1'; // parameterize the IP of the Listen var PORT = 6969; // TCP LISTEN port
 var PORT = 6969; // TCP LISTEN port
 
 var clients = []
 
-
-import { Client } from "./clients";
-
 net.createServer ((sock) =>{
 
     console.log('CONNECTED: ' + sock.remoteAddress +':'+ sock.remotePort);
 
-    var newclient = Client(sock.remoteAddress,sock.remotePort)
+    var newclient = Client(sock.remoteAddress, sock.remotePort)
     clients.push(newclient)
 
     sock.on('data', (data)=> {
